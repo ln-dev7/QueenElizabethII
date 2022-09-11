5f8eca4d54ffab9280838a2695e63f20531b9331
ln-dev7 <benzema.9>
2022-09-11 14:30:28 +0100

    hero section desktop

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // ----------------------------------------------------------------------------
 const loader = document.querySelector("#loader");
 window.addEventListener("load", () => {
-  loader.classList.add("loaded");
+    // setTimeout(() => {
+    //     loader.classList.add("loaded");
+    // }, 1000);
+    loader.classList.add("loaded");
 });
 // ----------------------------------------------------------------------------
-const menuClose = document.querySelector(".menu-close");
-const menuOpen = document.querySelector(".header-left-menu");
-const menu = document.querySelector(".menu");
+// const menuClose = document.querySelector(".menu-close");
+// const menuOpen = document.querySelector(".header-left-menu");
+// const menu = document.querySelector(".menu");
 
-menuOpen.addEventListener("click", () => {
-  menu.classList.add("menu-open");
-});
-menuClose.addEventListener("click", () => {
-  menu.classList.remove("menu-open");
-});
+// menuOpen.addEventListener("click", () => {
+//   menu.classList.add("menu-open");
+// });
+// menuClose.addEventListener("click", () => {
+//   menu.classList.remove("menu-open");
+// });
 // ----------------------------------------------------------------------------
